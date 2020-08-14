0baa7e09ed16522416f8008467fbb435a4630d4d
cruzerngz <61926974+cruzerngz@users.noreply.github.com>
2020-08-14 06:06:42 +0000

    Update sci-pap-stone.py

--- sci-pap-stone.py
+++ sci-pap-stone.py
@@ -38,6 +38,7 @@ while gamecount < prescribed_games:
         ##Get the user's play
         userinput = input("Turn " + str(gamecount + 1) + "\nChoose your [scissors,paper,stone]!\n")
         #Check for any errors in selection, then proceeds with game if true
+        #Now accepts shortform of the full word, as well as numbers corresponding to the position in list
         if userinput in choice1 + choice2 + choice3:
             print("you have chosen " + userinput + "!")
             #resets number of false attempts back to 0 for more shenanigans
@@ -46,7 +47,7 @@ while gamecount < prescribed_games:
         
         #Prints out a random string from the list below, and prompts a re-entry
         else:
-            insultlist = ["Don't play games with me","We're playing scissors paper stone here","what are you doing?","as a computer I can't believe I'm facepalming","do you know how the world works?","Do you know how to type?"]
+            insultlist = ["Um... I am out of words.","How are you so bad at this?","Don't play games with me","We're playing scissors paper stone here","what are you doing?","as a computer I can't believe I'm facepalming","do you know how the world works?","Do you know how to type?"]
             #added a self counting system so insults can be added any time
             insultcount = len(insultlist)
             insultindex = random.randint(0,insultcount - 1)

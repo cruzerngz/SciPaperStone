56f6f23456a77d0bb7b102f40fad67e4ac176183
cruzerngz <cruzerngz+github@gmail.com>
2021-02-01 13:18:00 +0800

    added function prototypes

--- C/SciPaperStone.c
+++ C/SciPaperStone.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+
+//functions needed for this program:
+//Time pause function (header)
+//random number generator (header)
+//testinput function (written), for checking the validity of a user's
+//command. Modifies a pointer input char and returns 0 if successful
+//returns -1 if unsuccessful
+//
+//insult function, spits out a string (written)
+
+int testinput();
+char throwinsult();
 void endgame();
 
 int main() {
